b58402abc51ad1a1bfd5d34dc721fb1b6a8c5b80
Son Nguyen <sontruongnguyen@kms-technology.com>
2025-04-10 14:15:21 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'c365-code-assignment';
+}
